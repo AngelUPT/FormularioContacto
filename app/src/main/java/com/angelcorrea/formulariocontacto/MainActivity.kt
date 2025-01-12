e09a3fefae1bdc9a7cce9b5ffba037ce1aaff4e4
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-12 10:48:14 -0600

    Cambios en textfields

--- app/src/main/java/com/angelcorrea/formulariocontacto/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/formulariocontacto/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.angelcorrea.formulariocontacto.ui.theme.FormularioContactoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +41,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text("Formulario de contacto", style = MaterialTheme.typography.bodyLarge, modifier = Modifier.align(Alignment.CenterHorizontally))
+        Text("Nombre:")
+        OutlinedTextField(value = "", onValueChange = {})
+        Text("Telefono:")
+        OutlinedTextField(value = "", onValueChange = {})
+        Text("Mensaje:")
+        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth().height(150.dp), singleLine = false, maxLines = 4)
+        Button(onClick = {}) {
+            Text("Enviar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
